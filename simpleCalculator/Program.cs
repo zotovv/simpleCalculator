bad3d4fd1ecc0ea9efe371dc66f1e525dfc4b7fd
Vladimir Zotov <zott14@outlook.com>
2018-03-16 10:41:13 +0300

    Calculator is created.

--- simpleCalculator/Program.cs
+++ simpleCalculator/Program.cs
@@ -10,6 +10,39 @@ namespace simpleCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите первое число: ");
+
+            double operand1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            double operand2 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите знак арифметического действия: ");
+            string sign = Console.ReadLine();
+
+            switch(sign)
+            {
+                case"+":
+                    Console.WriteLine(operand1 + operand2);
+                    break;
+                case"-":
+                    Console.WriteLine(operand1 - operand2);
+                    break;
+                case "*":
+                    Console.WriteLine(operand1 * operand2);
+                    break;
+                case "/":
+                    if (operand2 == 0)                  
+                        Console.WriteLine("Деление на ноль невозможно.");                       
+                    else
+                        Console.WriteLine(operand1 / operand2);
+                    break;
+                default:
+                    Console.WriteLine("Такая операция не поддерживается этой программой.");
+                    break;
+            }
+            Console.ReadKey();
+
         }
     }
 }
