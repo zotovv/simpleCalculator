a762f09956564864c95d17af93012ddbb5905a4e
Vladimir Zotov <zott14@outlook.com>
2018-03-17 10:43:57 +0300

    Update. Made a formative output.

---
+++ simpleCalculator/simple.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+
+            double operand1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            double operand2 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите знак арифметического действия: ");
+            string sign = Console.ReadLine();
+
+            switch(sign)
+            {
+                case"+":
+                    Console.WriteLine(operand1 + operand2);
+                    break;
+                case"-":
+                    Console.WriteLine(operand1 - operand2);
+                    break;
+                case "*":
+                    Console.WriteLine(operand1 * operand2);
+                    break;
+                case "/":
+                    if (operand2 == 0)                  
+                        Console.WriteLine("Деление на ноль невозможно.");                       
+                    else
+                        Console.WriteLine(operand1 / operand2);
+                    break;
+                default:
+                    Console.WriteLine("Такая операция не поддерживается этой программой.");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ simpleCalculator/simpleCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+
+            double operand1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            double operand2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите знак арифметического действия: ");
+            string sign = Console.ReadLine();
+
+            switch(sign)
+            {
+                case"+": Console.WriteLine("{0} + {1} = {2}", operand1, operand2, operand1 + operand2);
+                    break;
+                case"-": Console.WriteLine("{0} - {1} = {2}", operand1, operand2, operand1 - operand2);
+                    break;
+                case"*": Console.WriteLine("{0} * {1} = {2}", operand1, operand2, operand1 * operand2);
+                    break;
+                case "/":
+                    if (operand2 != 0)
+                    {
+                        Console.WriteLine("{0} / {1} = {2}", operand1, operand2, operand1 / operand2);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(operand1 / operand2);
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Вы ввели знак не арифметической операции.");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
